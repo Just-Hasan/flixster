2eb4938047756cca7b553f9a63881e7a714145e6
Just-Hasan-Desktop <109896294+Just-Hasan@users.noreply.github.com>
2024-04-04 19:24:12 +0700

    Update MovieItem in Flixster
    - Fix bug where the movie items is error when trying to redirect to selected tv / movie

--- src/ui/MovieItem.jsx
+++ src/ui/MovieItem.jsx
@@ -3,7 +3,26 @@ import { Link } from "react-router-dom";
 import { useLocation } from "react-router-dom";
 export default function MovieItem(movie) {
   const location = useLocation();
+  const mediaType = movie?.type;
   const type = location.pathname.split("/")[1];
+  if (type === "show") {
+    return (
+      <Link
+        to={`/${mediaType}/selected?title=${
+          movie?.movie.original_title || movie?.movie.name
+        }&type=${mediaType}&id=${movie?.movie.id}`}
+        key={movie?.movie.id}
+        className="overflow-hidden "
+      >
+        <div className="relative h-full w-full">
+          <img
+            src={`https://image.tmdb.org/t/p/original/${movie?.movie.poster_path}`}
+            className="aspect-[2/3]"
+          />
+        </div>
+      </Link>
+    );
+  }
   return (
     <Link
       to={`/${type}/selected?title=${
